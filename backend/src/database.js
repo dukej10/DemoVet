c13d8e2cf2af9a9d074cf4e8562b4a54206124a7
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-03-30 22:47:51 -0500

    conexión MongoAtlas

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,9 +1,12 @@
 // Conexion a la base de datos
 import mongoose from "mongoose";
 mongoose
-  .connect("mongodb://localhost/DemoVet", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://dukejmono:duke10245L@cluster0.dvmm5.mongodb.net/DemoVet?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then((db) => console.log("Db is connected"))
   .catch((erro) => console.log(error));
